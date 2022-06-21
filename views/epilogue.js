93a0d52e1b6ee6380dfe219cb64e619fb2bed0b9
Taejung Kim <immcoc1@gmail.com>
2022-06-21 16:59:57 +0900

    feat: temporary ending screen

--- views/epilogue.js
+++ views/epilogue.js
@@ -1,8 +1,10 @@
 let epilogueSceneNum = 0;
 
 function showEpilogue(){
-  showComputerBackground();
-  showHowToGoNext();
+  if(epilogueSceneNum <= 3){
+    showComputerBackground();
+    showHowToGoNext();
+  }
   
   if(epilogueSceneNum == 0){
     showDialogue("헉!");
@@ -12,12 +14,16 @@ function showEpilogue(){
     showDialogue("휴, 다시 내 방이구나....");
   }
   
-  if(epilogueSceneNum >= 2){
+  if(epilogueSceneNum >= 2 && epilogueSceneNum <= 3){
     showChecklist(200, 30);
   }
   
   if(epilogueSceneNum == 3){
     showDialogue("그래, 아무리 미로같이 막막해도 하나씩 차근차근 하다면 끝이 보이겠지. 힘내자!");
+  }
+  
+  if(epilogueSceneNum >= 4){
+    showEndingScreen();
   }
   
   if (isFadingOut) {
@@ -27,12 +33,26 @@ function showEpilogue(){
   }
 }
 
+function showEndingScreen(){
+  background(0);
+  textSize(40);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text("Thank You For Playing!", width/2, height/2- 40);
+  
+  textSize(15);
+  fill(150);
+  text("Made By\n\n김태정 이지수 타트아냐 이지수", width/2, height/2 + 50);
+  textSize(10);
+  text("아무 키를 눌러 시작 화면으로", width/2, height - 30);
+}
+
 function epilogueKeyPressed(k){
   epilogueSceneNum++;
   if (epilogueSceneNum == 2) {
       mouseSound.play();
     }
-  if (epilogueSceneNum == 4) {
+  if (epilogueSceneNum == 5) {
       startFadeOut(100);
   }
 }
